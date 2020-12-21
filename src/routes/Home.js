f66012ee638def04d345b7f8c2ed90246915664a
dhanush72 <dhanuraj7258@gmail.com>
2020-12-21 15:11:38 +0530

    display tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await db.collection("tweets").add({
@@ -17,16 +19,38 @@ const Home = () => {
     setTweet(value);
   };
 
+  const getTweets = async () => {
+    const dbTweets = await db.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const newObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [newObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        placeholder="start typing...."
-        value={tweet}
-        onChange={onChange}
-      />
-      <input type="submit" value="tweet" />
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="start typing...."
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+      {tweets.map((data) => (
+        <div>
+          <h4 key={data.id}>{data.tweet}</h4>
+        </div>
+      ))}
+    </>
   );
 };
 
